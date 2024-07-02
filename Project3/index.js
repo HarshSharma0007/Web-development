4781e383e8505d134abd1db5d09530aea19d8a0d
Harsh Sharma <153315733+HarshSharma0007@users.noreply.github.com>
2024-07-02 15:15:18 +0530

    index.js

--- Project3/index.js
+++ Project3/index.js
@@ -1,12 +1,9 @@
-// document.getElementById("count-el").innerText = 5
-
 let count = 0
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
 console.log(saveEl);
 function increment() {
     count+=1
-    //countEl.innerText = count;//instead of this
     countEl.textContent=count
 }
 function save(){
@@ -14,41 +11,4 @@ function save(){
     saveEl.innerText += countStr
     countEl.textContent= 0
     count=0
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let count = 0
-// let saveEl = document.getElementById("save-el")
-// let countEl = document.getElementById("count-el")
-
-// function increment() {
-//     count += 1
-//     countEl.textContent = count
-// }
-
-// function save() {
-//     let countStr = count + " - "
-//     saveEl.textContenttStr
-//     countEl.textContent = 0
-//     count = 0
-// }
-
-// console.log("Let's count people on the subway!")
-
-
-
